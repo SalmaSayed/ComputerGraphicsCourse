92283d208b9a1efff8ebc154ecc60da47e0c605f
SalmaSayed <28610405+SalmaSayed@users.noreply.github.com>
2018-10-24 01:38:25 +0200

    Add files via upload

---
+++ Assignment 2/Exp.h
@@ -0,0 +1,36 @@
+#ifndef __EQUATION_PARSER
+#define __EQUATION_PARSER
+
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+struct Term {
+public:
+	float exponent = 1;
+	float coef = 1;
+	string function = "";
+
+	float evalTrig(float x);
+	float eval(float x);
+	void print();
+};
+
+class Exp {
+
+private:
+	vector<Term> terms;
+	float string2number(string str);
+
+public:
+	void parse(string);
+	float eval(float);
+	void print();
+	~Exp();
+};
+
+#include "Exp.cpp"
+#endif
